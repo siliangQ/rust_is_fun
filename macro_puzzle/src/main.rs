edb6723b7afed28a0904893a780ab625b473a602
siliang <szhang@endiag.com>
2021-04-14 10:03:33 -0700

    init

---
+++ macro_puzzle/src/main.rs
@@ -0,0 +1,15 @@
+macro_rules! m {
+    ($($s:stmt)*) => {
+        $(
+            {stringify!($s); 1}
+        )<<*
+    }
+}
+fn main() {
+    println!(
+        "{}{}{}",
+        m!{return || true},
+        m! { (return) || true},
+        m! { {return} || true}
+    )
+}
